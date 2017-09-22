c3de4e561abea893240ec28ba42524619971b377
Cleverson Lezainski <cleversonlezainski@gmail.com>
2017-09-22 18:15:18 -0300

    t
    t

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MaterializeModule } from 'angular2-materialize';
+import { HeaderComponent } from './header/header.component';
+import { FootnoteComponent } from './footnote/footnote.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FootnoteComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
